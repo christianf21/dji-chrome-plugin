2e8abf0414173e73bb3d2b5aecffdaec518032d9
Christian Feo <cfeo@GALPAS.com>
2016-09-22 16:47:44 -0400

    creada funcion que  detecta el td necesario para agarrar los datos a sumar

--- stockcounter.js
+++ stockcounter.js
@@ -10,48 +10,85 @@
 
 var stockLocation = "https://es-us.finanzas.yahoo.com/q/cp?s=%5EDJI";
 
-//$("body").css("background-color","blue");
 
-// check if <td> contains <img>, if it does, check if red or green and count each.
+//operate();
+test();
 
-var red = "#cc0000;";
-var green = "#008800;";
 
-var redCount = 0;
-var greenCount = 0;
-var blackCount = 0;
+function operate(){
 
-// used for calculating averages
-var greenNums = new Array();
-var redNums = new Array();
+	reload = setTimeout(function(){
+		   //location.reload(true);
+		   console.log("testing.....");
+		   operate();
+		}, 6000);
 
-var items = $("td.yfnc_tabledata1").has("img"); // the <td> that has <img>
+	countStock();
+	injectHtml();
+}
 
-//var floatRegex = '[-+]?([0-9]*.[0-9]+|[0-9]+)'; 
+function test(){
+	countStock2();
+}
 
-$.each(items, function(){ 
+function countStock2(){
 
-	var color = $(this).find('b:first').attr('style').split(':')[1];
+	items = $("td.yfnc_tabledata1"); // the <td> that has <img>
 
-	var temp =  $(this).find('b:last').text();
-	var value = temp.substr(2,4);
+	var amount = 0;
 
-	if(color === red){
-		
-		redCount++;
-		redNums.push( parseFloat( value ) ); // add first <b> values to array for processing later
+	$.each(items, function(){
 
-	} else if (color === green){
-		
-		greenCount++;
-		greenNums.push( parseFloat( value ) );
-	}
+		var cant = $(this).children('span');
+		if(cant.length == 2)
+			amount += 1;
+	});
 
-});
+	console.log("Rows = " + amount);
+}
 
-// Calculating Green VS Red averages
-var greenAVG = 0;
-var redAVG = 0;
+function countStock() {
+
+	red = "#cc0000;";
+	green = "#008800;";
+
+	redCount = 0;
+	greenCount = 0;
+	blackCount = 0;
+
+	// used for calculating averages
+	greenNums = new Array();
+	redNums = new Array();
+
+	items = $("td.yfnc_tabledata1").has("span#yfs_c63_aapl"); // the <td> that has <img>
+
+	console.log("Data = " + items.length);
+
+	return;
+	// check if <td> contains <img>, if it does, check if red or green and count each.
+	$.each(items, function(){ 
+
+		var color = $(this).find('b:first').attr('style').split(':')[1];
+
+		var temp =  $(this).find('b:last').text();
+		var value = temp.substr(2,4);
+
+		if(color === red){
+			
+			redCount++;
+			redNums.push( parseFloat( value ) ); // add first <b> values to array for processing later
+
+		} else if (color === green){
+			
+			greenCount++;
+			greenNums.push( parseFloat( value ) );
+		}
+
+	});
+
+	// Calculating Green VS Red averages
+	greenAVG = 0;
+	redAVG = 0;
 
 
 	// GREENs
@@ -72,36 +109,16 @@ var redAVG = 0;
 	}
 
 	redAVG = totalRed / redNums.length;
+}
 
-/*****************************************/
+function injectHtml(){
 
+	blackCount = 30 - (greenCount + redCount);
 
-blackCount = 30 - (greenCount + redCount);
-
-$("#marketindices").append("<h1>Index Count:   <span style='color:green;'>" + greenCount +
-			
-					 "<span style='font-size:20px;'>("+greenAVG.toFixed(2)+"%)</span></span> vs <span style='color:red;'>" + 
-
-					 redCount + "<span style='font-size:20px;'>("+redAVG.toFixed(2)+"%)</span></span> " + 
-
-					 (blackCount > 0 ? " ("+blackCount+")" : "") + "</h1>");
-
-$("#marketindices").append("<input type='radio' id='autoref' name='group1' value='autorefresh' checked> Auto Refresh <br>" +
-
-							"<input type='radio' id='pausa' name='group1' value='pausa'> Pausa");
-
-var reload = setTimeout(function(){
-		   location.reload(true);
-		}, 6000);
-
-$("input:radio[name=group1]").change( function() {
-
-	if(this.value === "pausa"){
-
-		clearTimeout(reload);	
-	} else {
-		reload = setTimeout(function(){
-		   location.reload(true);
-		}, 6000);
-	}
-});
+	$("#companynav").html(
+		"<h1>Index Count:   <span style='color:green;'>" + greenCount +
+	 	"<span style='font-size:20px;'>("+greenAVG.toFixed(2)+"%)</span></span> vs <span style='color:red;'>" + 
+		 redCount + "<span style='font-size:20px;'>("+redAVG.toFixed(2)+"%)</span></span> " + 
+		 (blackCount > 0 ? " ("+blackCount+")" : "") + "</h1>"
+	 );
+}
